250736e29b3e09104d378bd2f1973a5067db83d4
murugadoss <devapps.smartrobos@gmail.com>
2023-12-14 21:39:02 +0530

    select_device_type added

---
+++ device.py
@@ -0,0 +1,35 @@
+import random
+import torch
+
+
+# intel_extension_for_pytorch
+
+def select_device_type():
+    # random seed
+    try:
+        if torch.cuda.is_available():
+            print("GPU is available")
+            seed = 88
+            random.seed(seed)
+            torch.cuda.manual_seed(seed)
+            torch.cuda.manual_seed_all(seed)
+            return torch.device("gpu")
+        elif torch.xpu.is_available():
+            print("GPU is not available")
+            print("XPU is available")
+            seed = 88
+            random.seed(seed)
+            torch.xpu.manual_seed(seed)
+            torch.xpu.manual_seed_all(seed)
+            return torch.device("xpu")
+        else:
+            print("XPU or GPU not available - returning with CPU")
+            return torch.device("cpu")
+    except Exception as err:
+        print(f"Error occurred details: {err}")
+        print("Returning with CPU")
+        return torch.device("cpu")
+
+
+if __name__ == '__main__':
+    select_device_type()
---
+++ main.py
@@ -0,0 +1,5 @@
+from device import select_device_type
+
+if __name__ == '__main__':
+    device_type = select_device_type()
+    print(device_type)
