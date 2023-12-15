4f102fa707d290d5b5fd77f69c77e3c11f862910
murugadoss <devapps.smartrobos@gmail.com>
2023-12-15 17:10:14 +0530

    updated chat_bot_model - removed unwanted variables

--- chatbot_model.py
+++ chatbot_model.py
@@ -36,8 +36,6 @@ class ChatBotModel:
         self.torch_dtype = torch_dtype
         self.device = get_device_type()
         self.model_id_or_path = model_id_or_path
-        # self.local_model_id = self.model_id_or_path.replace("/", "--")
-        # self.local_model_path = os.path.join(MODEL_CACHE_PATH, self.local_model_id)
         self.autocast = get_autocast(self.device)
         self.torch_dtype = torch_dtype
 
@@ -45,17 +43,6 @@ class ChatBotModel:
 
         self.tokenizer_init(self.model_id_or_path)
         self.model_init(self.model_id_or_path)
-
-        # try:
-        #     info(f"AutoTokenizer.from_pretrained local_model_path : {self.local_model_path} ")
-        #     # Initialize the tokenizer and base model for text generation
-        #     self.tokenizer_init(self.local_model_path)
-        #     self.model_init(self.local_model_path)
-        # except (OSError, ValueError, EnvironmentError) as e:
-        #     info(f"Tokenizer / model not found locally. "
-        #          f"Downloading tokenizer / model for {self.model_id_or_path} to cache...: ")
-        #     self.tokenizer_init(self.model_id_or_path)
-        #     self.model_init(self.model_id_or_path)
 
         if optimize:
             info(f"Enabling Intel ipex Optimizer")
@@ -95,8 +82,6 @@ class ChatBotModel:
         info(f"torch_dtype : {self.torch_dtype}")
         info(f"device : {self.device}")
         info(f"model_id_or_path : {self.model_id_or_path}")
-        # info(f"local_model_id : {self.local_model_id}")
-        # info(f"local_model_path : {self.local_model_path}")
 
 
 if __name__ == '__main__':
