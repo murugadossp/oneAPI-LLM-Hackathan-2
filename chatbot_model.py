be32d4289a261f1862c822c32b6b14485726aabb
murugadoss <devapps.smartrobos@gmail.com>
2023-12-15 16:42:23 +0530

    updated chat_bot_model

--- chatbot_model.py
+++ chatbot_model.py
@@ -1,8 +1,5 @@
 # Import necessary modules and classes
-import os
-
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-import torch
 import intel_extension_for_pytorch as ipex
 from device import *
 from loggers import *
@@ -39,23 +36,26 @@ class ChatBotModel:
         self.torch_dtype = torch_dtype
         self.device = get_device_type()
         self.model_id_or_path = model_id_or_path
-        self.local_model_id = self.model_id_or_path.replace("/", "--")
-        self.local_model_path = os.path.join(MODEL_CACHE_PATH, self.local_model_id)
+        # self.local_model_id = self.model_id_or_path.replace("/", "--")
+        # self.local_model_path = os.path.join(MODEL_CACHE_PATH, self.local_model_id)
         self.autocast = get_autocast(self.device)
         self.torch_dtype = torch_dtype
 
         self.print_params()
 
-        try:
-            info(f"AutoTokenizer.from_pretrained local_model_path : {self.local_model_path} ")
-            # Initialize the tokenizer and base model for text generation
-            self.tokenizer_init(self.local_model_path)
-            self.model_init(self.local_model_path)
-        except (OSError, ValueError, EnvironmentError) as e:
-            info(f"Tokenizer / model not found locally. "
-                 f"Downloading tokenizer / model for {self.model_id_or_path} to cache...: ")
-            self.tokenizer_init(self.model_id_or_path)
-            self.model_init(self.model_id_or_path)
+        self.tokenizer_init(self.model_id_or_path)
+        self.model_init(self.model_id_or_path)
+
+        # try:
+        #     info(f"AutoTokenizer.from_pretrained local_model_path : {self.local_model_path} ")
+        #     # Initialize the tokenizer and base model for text generation
+        #     self.tokenizer_init(self.local_model_path)
+        #     self.model_init(self.local_model_path)
+        # except (OSError, ValueError, EnvironmentError) as e:
+        #     info(f"Tokenizer / model not found locally. "
+        #          f"Downloading tokenizer / model for {self.model_id_or_path} to cache...: ")
+        #     self.tokenizer_init(self.model_id_or_path)
+        #     self.model_init(self.model_id_or_path)
 
         if optimize:
             info(f"Enabling Intel ipex Optimizer")
@@ -74,8 +74,7 @@ class ChatBotModel:
     def tokenizer_init(self, model_id_or_path):
         info(f"AutoTokenizer.from_pretrained model_id_or_path : {model_id_or_path} ")
         self.tokenizer = AutoTokenizer.from_pretrained(
-            model_id_or_path, trust_remote_code=True,
-            cache_dir=MODEL_CACHE_PATH
+            model_id_or_path, trust_remote_code=True
         )
 
     def model_init(self, model_id_or_path):
@@ -86,8 +85,7 @@ class ChatBotModel:
                 device_map="auto",
                 low_cpu_mem_usage=True,
                 trust_remote_code=True,
-                torch_dtype=torch.float32,
-                cache_dir=MODEL_CACHE_PATH
+                torch_dtype=torch.float32
             )
             .to(self.device)
             .eval()
@@ -97,8 +95,8 @@ class ChatBotModel:
         info(f"torch_dtype : {self.torch_dtype}")
         info(f"device : {self.device}")
         info(f"model_id_or_path : {self.model_id_or_path}")
-        info(f"local_model_id : {self.local_model_id}")
-        info(f"local_model_path : {self.local_model_path}")
+        # info(f"local_model_id : {self.local_model_id}")
+        # info(f"local_model_path : {self.local_model_path}")
 
 
 if __name__ == '__main__':
